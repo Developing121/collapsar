d4e916fe444e4e285d874c017901a9517a60c16d
Fengxd <2467298924@qq.com>
2024-02-20 09:34:59 +0800

    生成静态文件

--- collapsar-generator-basic/src/main/java/com/feng/Main.java
+++ collapsar-generator-basic/src/main/java/com/feng/Main.java
@@ -1,26 +1,37 @@
 package com.feng;
 
+import com.feng.utils.FileUtil;
+
+import java.io.File;
+import java.io.IOException;
+
 /**
 * ClassName: ${NAME}
 * Package: com.feng
 * Description: 
 * @Author Fengxd
 * @Create 2024/2/18 21:29 
-* @Version 1.0   
-*/// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+* @Version 1.0
+*/
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        /*
+        静态文件生成
+        1.获取输入文件路径
+        2.获取输出文件路径
+        3.使用 hutool copy 文件
+         */
+        // 获取整个项目的根路径
+        String projectPath = System.getProperty("user.dir");
+        File parentFile = new File(projectPath);
+        // 输入路径
+        String inputPath = new File(parentFile, "collapsar-generator-demo-projects/acm-template").getAbsolutePath();
+        // 输出路径
+        String outputPath = projectPath;
+        try {
+            FileUtil.copy(new File(inputPath), parentFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }

749cbd6146839860cf8fb036348bfa7311ef59e3
Fengxd <2467298924@qq.com>
2024-02-24 15:33:26 +0800

    修改 jar 生成的位置

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # Collapsar 定制化本地代码生成器
 持续更新中~
 
-在本地生成一个定制化炫酷黑洞网页。
+## 介绍
+一个基本的代码生成器,允许通过用户指定的参数,在本地生成一个定制化的炫酷黑洞网页。
+
+## 目录结构
+| 目录                        | 描述    |
+|---------------------------|-------|
+| collapsar-demo            | 草稿    |
+| collapsar-generator-basic | 代码生成器 |
+| collapsar-generator-template    | 模板    |
+
+## 技术选型
+- picocli
+- freemarker
+
+## 使用
+1.进入 collapsar-generator-basic 目录,执行命令:
+```shell
+mvn clean package
+```
 
 ## 在命令行中运行中文乱码的问题
 可以通过 JVM 的默认编码格式来解决
--- collapsar-generator-basic/src/test/java/com/feng/generate/FileGeneratorTest.java
+++ collapsar-generator-basic/src/test/java/com/feng/generate/FileGeneratorTest.java
@@ -15,18 +15,18 @@ import org.junit.Test;
 public class FileGeneratorTest {
     @Test
     public void testGenerate() {
-        CollapsarTemplateModel model = new CollapsarTemplateModel();
-        model.setAuthor("Fengxd");
-        model.setTitle("WebGL 网页黑洞特效");
-        model.setDescription("一个炫酷的 WebGL 网页黑洞特效");
-        model.setBackground("https://images.unsplash.com/photo-1462331940025-496dfbfc7564?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1427&q=80");
-        model.setMass(1500);
-        model.setAutoMove(true);
-        model.setTextSize("16px");
-        model.setTextColor("#000000");
-        model.setMouseLeaveMove(false);
-        model.setMouseMove(true);
-        model.setClick(true);
-        FileGenerator.generate(model);
+//        CollapsarTemplateModel model = new CollapsarTemplateModel();
+//        model.setAuthor("Fengxd");
+//        model.setTitle("WebGL 网页黑洞特效");
+//        model.setDescription("一个炫酷的 WebGL 网页黑洞特效");
+//        model.setBackground("https://images.unsplash.com/photo-1462331940025-496dfbfc7564?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1427&q=80");
+//        model.setMass(1500);
+//        model.setAutoMove(true);
+//        model.setTextSize("16px");
+//        model.setTextColor("#000000");
+//        model.setMouseLeaveMove(false);
+//        model.setMouseMove(true);
+//        model.setClick(true);
+//        FileGenerator.generate(model);
     }
 }
